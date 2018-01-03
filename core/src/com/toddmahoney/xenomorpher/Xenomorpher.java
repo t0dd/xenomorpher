e48164c74450f8c773a803eb41d8639a37dfb7cd
Todd Mahoney <todd.developer@gmail.com>
2018-01-03 12:41:51 -0700

    setting up main game class, and views for switching between screens

--- core/src/com/toddmahoney/xenomorpher/Xenomorpher.java
+++ core/src/com/toddmahoney/xenomorpher/Xenomorpher.java
@@ -1,33 +1,49 @@
 package com.toddmahoney.xenomorpher;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
+import com.toddmahoney.xenomorpher.views.EndScreen;
+import com.toddmahoney.xenomorpher.views.MainScreen;
+import com.toddmahoney.xenomorpher.views.MenuScreen;
+import com.toddmahoney.xenomorpher.views.PreferencesScreen;
+import com.toddmahoney.xenomorpher.views.LoadingScreen;
 
-public class Xenomorpher extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+public class Xenomorpher extends Game {
+
+	public final static int MENU = 0;
+	public final static int PREFERENCES = 1;
+	public final static int APPLICATION = 2;
+	public final static int ENDGAME = 3;
+
+	private LoadingScreen loadingScreen;
+	private PreferencesScreen preferencesScreen;
+	private MenuScreen menuScreen;
+	private MainScreen mainScreen;
+	private EndScreen endScreen;
+
+	public void changeScreen(int screen) {
+		switch(screen){
+			case MENU:
+				if(menuScreen == null) menuScreen = new MenuScreen();
+					this.setScreen(menuScreen);
+				break;
+			case PREFERENCES:
+				if(preferencesScreen == null) preferencesScreen = new PreferencesScreen();
+					this.setScreen(preferencesScreen );
+				break;
+			case APPLICATION:
+				if(mainScreen == null) mainScreen = new MainScreen();
+					this.setScreen(mainScreen);
+				break;
+			case ENDGAME:
+				if(endScreen == null) endScreen = new EndScreen();
+					this.setScreen(endScreen);
+				break;
+		}
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void create() {
+		loadingScreen = new LoadingScreen();
+		setScreen(loadingScreen);
 	}
 }
