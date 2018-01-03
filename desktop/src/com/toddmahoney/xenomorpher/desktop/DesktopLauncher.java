3cc46a0859f7b15c587ebda7a6e7f0b59f10670d
t0dd <todd.developer@gmail.com>
2018-01-03 02:11:08 -0700

    Initial commit

---
+++ desktop/src/com/toddmahoney/xenomorpher/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.toddmahoney.xenomorpher.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.toddmahoney.xenomorpher.Xenomorpher;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Xenomorpher(), config);
+	}
+}
