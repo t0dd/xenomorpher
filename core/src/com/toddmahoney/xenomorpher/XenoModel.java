a8b7080dbf6ea2eb6afb4d09a3b296c0d48ba535
Todd Mahoney <todd.developer@gmail.com>
2018-01-06 17:27:07 -0700

    playing around with Box2D physics bodies

--- core/src/com/toddmahoney/xenomorpher/XenoModel.java
+++ core/src/com/toddmahoney/xenomorpher/XenoModel.java
@@ -7,10 +7,15 @@ import com.badlogic.gdx.physics.box2d.*;
 public class XenoModel {
 
     public World world;
-    private Body bodyd;
+    private Body bodyd; //Box2D Dynamic body
+    private Body bodys; //Box2D Static body
+    private Body bodyk; //Box2D Kinematic body
 
     public XenoModel() {
         world = new World(new Vector2(0, -10f), true);
+        createFloor();
+        createObject();
+        createMovingObject();
     }
 
     //game logic
@@ -46,5 +51,57 @@ public class XenoModel {
 
         // we no longer use the shape object here so dispose of it.
         shape.dispose();
+    }
+
+    private void createFloor() {
+
+        // create a new body definition (type and location)
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+        bodyDef.position.set(0, -10);
+
+        // add it to the world
+        bodys = world.createBody(bodyDef);
+
+        // set the shape (here we use a box 50 meters wide, 1 meter tall )
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(50, 1);
+
+        // create the physical object in our body)
+        // without this our body would just be data in the world
+        bodys.createFixture(shape, 0.0f);
+
+        // we no longer use the shape object here so dispose of it.
+        shape.dispose();
+    }
+
+    private void createMovingObject(){
+
+        //create a new body definition (type and location)
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.KinematicBody;
+        bodyDef.position.set(0,-12);
+
+
+        // add it to the world
+        bodyk = world.createBody(bodyDef);
+
+        // set the shape (a box 50 meters wide, 1 meter tall )
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(1,1);
+
+        // set the properties of the object ( shape, weight, restitution(bouncy-ness)
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = 1f;
+
+        // create the physical object in our body)
+        // without this our body would just be data in the world
+        bodyk.createFixture(shape, 0.0f);
+
+        // we no longer use the shape object here so dispose of it.
+        shape.dispose();
+
+        bodyk.setLinearVelocity(0, 0.75f);
     }
 }
